28f758794f3f44970f6d3675c72801fb204b7133
Anamika2408 <76700732+Anamika2408@users.noreply.github.com>
2025-02-17 20:43:47 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import FunctionalComponent from './Components/FunctionalComponent';
+import ClassComponent from './Components/ClassComponent';
+
+function App() {
+  const element = <h1>Hello, World!</h1>;
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+      <FunctionalComponent/>
+      <ClassComponent/>
+      {element}
+    </div>
+  );
+}
+
+export default App;
